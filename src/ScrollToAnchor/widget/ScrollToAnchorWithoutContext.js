804af7b349a6caf16180cae52d7e67545f1b7244
703180248 <703180248@703180248GDL.IND.CORP.AD>
2018-07-20 17:28:59 +0530

    related to Jquery version changes for prevention of XSS scripting

--- src/ScrollToAnchor/widget/ScrollToAnchorWithoutContext.js
+++ src/ScrollToAnchor/widget/ScrollToAnchorWithoutContext.js
@@ -35,7 +35,7 @@ define([
     "dojo/html",
     "dojo/_base/event",
 
-    "ScrollToAnchor/lib/jquery-1.11.2"
+    "ScrollToAnchor/lib/jquery-1.12.1"
 ], function(declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoEvent, _jQuery) {
     "use strict";
 
